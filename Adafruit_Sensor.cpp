137b67db4569db9f1ed4df2d17b75a8b648390f5
lady ada <limor@ladyada.net>
2022-07-31 16:01:00 -0400

    printy!

--- Adafruit_Sensor.cpp
+++ Adafruit_Sensor.cpp
@@ -61,6 +61,9 @@ void Adafruit_Sensor::printSensorDetails(void) {
   case SENSOR_TYPE_COLOR:
     Serial.print(F("Color (RGBA)"));
     break;
+  case SENSOR_TYPE_TVOC:
+    Serial.print(F("Total Volatile Organic Compounds (ppb)"));
+    break;
   }
 
   Serial.println();
