5647e2cc296c19b0ac04676ee7a4a1a2e703fe5f
Doxygen CI <ci-arduino@invalid>
2022-12-20 17:50:26 +0000

    Deploy docs to GitHub Pages from commit 6ac3e2df0f
    Commit: 6ac3e2df0f095e554159a7e0880e3aa8dc693feb
    GitHub Actions run: 3742882025

--- html/search/variables_8.js
+++ html/search/variables_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['name',['name',['../structsensor__t.html#a4383b68d36f9dc4096d83583c3b009bf',1,'sensor_t']]]
+  ['name',['name',['../structsensor__t.html#a4383b68d36f9dc4096d83583c3b009bf',1,'sensor_t']]],
+  ['nox_5findex',['nox_index',['../structsensors__event__t.html#a62a19b2dbd9cad0f9dc746749741eb59',1,'sensors_event_t']]]
 ];
